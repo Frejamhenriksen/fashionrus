5d79b055e42d80f6a601e356764f0ae289998bfe
Frejamaria <frhe0004@stud.kea.dk>
2025-09-29 12:47:31 +0200

    alle mulige ændringer

--- js/productlist.js
+++ js/productlist.js
@@ -1,6 +1,6 @@
 const productListContainer = document.querySelector("#productListContainer");
 
-fetch(`https://kea-alt-del.dk/t7/api/products?limit=10`)
+fetch(`https://kea-alt-del.dk/t7/api/products/?limit=100`)
   .then((response) => response.json())
   .then((data) => showProducts(data));
 
@@ -9,13 +9,11 @@ function showProducts(products) {
   products.forEach((element) => {
     console.log(element);
     productListContainer.innerHTML += `
-               <div class="card sold-out">
+               <div class="card soldout ${element.soldout && "soldOut"}>
           <div class="card-img">
-            <a href="product.html"><img src="img/1164-img.webp" alt="Nummer 1573" /></a>
+            <a href="product.html"><img src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp" alt="product image" /></a>
+            <element class="sold-out-badge">Sold Out</element>
             <div class="sold-out-overlay"></div>
-            <div class="sold-out-badge">Sold Out</div>
-          </div>
-          <div>
             <h3>Sahara Team India Fanwear Round Neck Jersey</h3>
             <p class="category">Tshirts | Nike</p>
             <p class="old-price">DKK 1595</p>
